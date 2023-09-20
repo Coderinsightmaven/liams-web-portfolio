5ac53c18f23be8c5eeb675a5359046022f42ba2e
Coderinsightmaven <lmarincik@proton.me>
2023-09-20 17:02:08 -0600

    added links to the navbar buttons

--- src/components/footer/page.tsx
+++ src/components/footer/page.tsx
@@ -5,7 +5,6 @@ export default function Footer() {
         <a className="link link-hover">About us</a>
         <a className="link link-hover">Contact</a>
         <a className="link link-hover">Jobs</a>
-        <a className="link link-hover">Press kit</a>
       </nav>
     </footer>
   );
--- src/components/navbar/page.tsx
+++ src/components/navbar/page.tsx
@@ -1,49 +1,20 @@
+import Link from "next/link";
+
 export default function Navbar() {
   return (
-    <div className="navbar bg-base-100">
-      <div className="navbar-start">
-        <div className="dropdown">
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
-          >
-            <li>
-              <a>About Me</a>
-            </li>
-            <li>
-              <a>Experience</a>
-            </li>
-            <li>
-              <a>Contact Me</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-      <div className="navbar-center hidden lg:flex">
+    <div className="navbar bg-base-100 flex justify-center items-center">
+      <div className="flex">
         <ul className="menu menu-horizontal px-1">
           <li>
-            <a>Item 1</a>
-          </li>
-          <li tabIndex={0}>
-            <details>
-              <summary>Parent</summary>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </details>
+            <Link href="/contact">Contact</Link>
           </li>
           <li>
-            <a>Item 3</a>
+            <Link href="/experience">Experience</Link>
+          </li>
+          <li>
+            <Link href="/about">About</Link>
           </li>
         </ul>
-      </div>
-      <div className="navbar-end">
-        <a className="btn">Button</a>
       </div>
     </div>
   );
